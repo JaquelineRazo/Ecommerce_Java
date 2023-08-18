907c194ddc0fb351f0389fab77d68fdf1b00f59b
JaquelineRazo <jaquelinerazo.ordaz@gmail.com>
2023-08-17 20:38:51 -0600

    Mapeo de tablas en SQL

--- src/main/java/com/rbn/ecommerce/model/Orden.java
+++ src/main/java/com/rbn/ecommerce/model/Orden.java
@@ -1,17 +1,40 @@
 package com.rbn.ecommerce.model;
+
 import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ordenes")
 public class Orden {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String numero;
     private Date fechaCreacion;
     private Date fechaRecibida;
+
     private double total;
+
+    @ManyToOne
+    private Usuario usuario;
+
+    @OneToMany(mappedBy = "orden")
+    private List<DetalleOrden> detalle;
 
     public Orden() {
 
     }
 
     public Orden(Integer id, String numero, Date fechaCreacion, Date fechaRecibida, double total) {
+        super();
         this.id = id;
         this.numero = numero;
         this.fechaCreacion = fechaCreacion;
@@ -59,14 +82,29 @@ public class Orden {
         this.total = total;
     }
 
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+
+    public List<DetalleOrden> getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(List<DetalleOrden> detalle) {
+        this.detalle = detalle;
+    }
+
     @Override
     public String toString() {
-        return "Orden{" +
-                "id=" + id +
-                ", numero='" + numero + '\'' +
-                ", fechaCreacion=" + fechaCreacion +
-                ", fechaRecibida=" + fechaRecibida +
-                ", total=" + total +
-                '}';
+        return "Orden [id=" + id + ", numero=" + numero + ", fechaCreacion=" + fechaCreacion + ", fechaRecibida="
+                + fechaRecibida + ", total=" + total + "]";
     }
+
+
 }
